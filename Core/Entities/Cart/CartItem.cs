65c7c59736532e8aae32eca85b00742e62874f48
adrian-lys-dev <drugakadrian@gmail.com>
2025-07-26 10:53:17 +0300

    CartItem additional value

--- Core/Entities/Cart/CartItem.cs
+++ Core/Entities/Cart/CartItem.cs
@@ -4,6 +4,7 @@
     {
         public int BookId { get; set; }
         public required string BookName { get; set; }
+        public required string AuthorName { get; set; }
         public decimal Price { get; set; }
         public int Quantity { get; set; }
         public required string PictureURL { get; set; }

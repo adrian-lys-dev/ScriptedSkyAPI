c83f08057d60bbf1e0997e24b34c8b288d41b6fd
adrian-lys-dev <drugakadrian@gmail.com>
2025-08-15 17:12:45 +0300

    admin controller, basic setup

---
+++ API/Controllers/AdminControllers/AdminCrudControllers/AdminPublisherController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers.AdminControllers.AdminCrudControllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminPublisherController : ControllerBase
+    {
+    }
+}
